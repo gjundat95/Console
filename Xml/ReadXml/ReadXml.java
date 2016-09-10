85fd49839a30a473b5f3d7fbf5f2cebe5172436a
gjundat95 <gjundat95@gmail.com>
2016-09-10 17:55:28 +0700

    add Xml Read file

---
+++ Xml/ReadXml/ReadXml.java
@@ -0,0 +1,43 @@
+package com.jundat95.XML;
+
+import java.io.File;
+
+import javax.xml.parsers.DocumentBuilder;
+import javax.xml.parsers.DocumentBuilderFactory;
+
+import org.w3c.dom.Document;
+import org.w3c.dom.Element;
+import org.w3c.dom.Node;
+import org.w3c.dom.NodeList;
+
+public class ReadXml {
+	public static void main(String args[]) {
+
+		try {
+
+			File input = new File("class.xml");
+			DocumentBuilderFactory dbFactory = DocumentBuilderFactory.newInstance();
+			DocumentBuilder dBuilder = dbFactory.newDocumentBuilder();
+			Document doc = (Document) dBuilder.parse(input);
+			doc.getDocumentElement().getNodeName();
+			System.out.println("Root element :" + doc.getDocumentElement().getNodeName());
+			NodeList nList = doc.getElementsByTagName("student");
+			for (int temp = 0; temp < nList.getLength(); temp++) {
+				System.out.println("---------------------------");
+				Node nNode = nList.item(temp);
+				System.out.println("Current element: " + nNode.getNodeName());
+				if (nNode.getNodeType() == Node.ELEMENT_NODE) {
+					Element eElement = (Element) nNode;
+					System.out.println("Student roll no:  " + eElement.getAttribute("rollno"));
+					System.out.println("First name: " + eElement.getElementsByTagName("firstname").item(0).getTextContent());
+					System.out.println("Last Name : " + eElement.getElementsByTagName("lastname").item(0).getTextContent());
+					System.out.println("Nick Name : " + eElement.getElementsByTagName("nickname").item(0).getTextContent());
+					System.out.println("Marks : " + eElement.getElementsByTagName("marks").item(0).getTextContent());
+				}
+			}
+
+		} catch (Exception ex) {
+			ex.printStackTrace();
+		}
+	}
+}
